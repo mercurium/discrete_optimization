94a212f39cdc0a2ba5b2c9f60ada1b0f678bc9df
mercurium <jerrychen434@gmail.com>
2013-08-17 19:44:16 -0700

    wrote simplistic facility location code

--- warehouse/solver.py
+++ warehouse/solver.py
@@ -55,15 +55,15 @@ def solveIt(inputData):
 	capacityRemaining = [w[0] for w in warehouses]
 
 	warehouseIndex = 0
-	for c in range(0, customerCount):
-		if capacityRemaining[warehouseIndex] >= customerSizes[c]:
-			solution[c] = warehouseIndex
-			capacityRemaining[warehouseIndex] -= customerSizes[c]
-		else:
-			warehouseIndex += 1
-			assert capacityRemaining[warehouseIndex] >= customerSizes[c]
-			solution[c] = warehouseIndex
-			capacityRemaining[warehouseIndex] -= customerSizes[c]
+	#for c in range(0, customerCount):
+	#	if capacityRemaining[warehouseIndex] >= customerSizes[c]:
+	#		solution[c] = warehouseIndex
+	#		capacityRemaining[warehouseIndex] -= customerSizes[c]
+	#	else:
+	#		warehouseIndex += 1
+	#		assert capacityRemaining[warehouseIndex] >= customerSizes[c]
+	#		solution[c] = warehouseIndex
+	#		capacityRemaining[warehouseIndex] -= customerSizes[c]
 
 	################################################### 
 	############### Above is naive solution ###########
@@ -71,31 +71,105 @@ def solveIt(inputData):
 
 	usage_count = dict()   ### Keeping track of which warehouses are used.
 	for i in range(warehouseCount): usage_count[i] = 0
-	for i in solution: usage_count[i] += 1
+	#for i in solution: usage_count[i] += 1
 
-	for c in xrange(customerCount):
-		cust = customerCosts[c]
+	#this setup seems to be hurting my later solution... o.O;;
+	#for iteration in range(3): #just in case a new place opens up that's better...
+	#	for c in xrange(customerCount):
+	#		cust = customerCosts[c]
+	#		current_cost = cust[solution[c]]
+	#		if usage_count[solution[c]] == 1:
+	#			current_cost += warehouses[solution[c]][1]
 
-		current_cost = cust[solution[c]]
-		if usage_count[solution[c]] == 1:
-			current_cost += warehouses[solution[c]][1]
+	#		for w in xrange(warehouseCount):
+	#			new_cost = cust[w] + (warehouses[w][1] if usage_count[w] == 0 else 0)
+	#			if new_cost <  current_cost and capacityRemaining[w] > customerSizes[c]:
+	#				solution[c] = w
+	#				capacityRemaining[w] -= customerSizes[c]
+	#				usage_count[w] +=1
 
+
+
+
+	###################################################	
+	###################################################	
+	###################################################	
+	
+	for iteration in range(100):
+		improved = False
+		improvements = []
+		for w in range(warehouseCount): 
+			improvements += [[0] * customerCount]
+			#improvements += [[ (customerCosts[c][solution[c]] - customerCosts[c][w] )  for c in range(customerCount) ] ]
+			for c in xrange(customerCount):
+				if solution[c] == -1:
+					improvements[w][c] = 10**5 - customerCosts[c][w]
+				elif solution[c] == w:
+					improvements[w][c] = 0
+					continue
+				else:
+					improvements[w][c] = customerCosts[c][solution[c]] - customerCosts[c][w] 
+				improvements[w][c] = max(improvements[w][c],0)
+	
 		for w in xrange(warehouseCount):
-			new_cost = cust[w] + (warehouses[w][1] if usage_count[w] == 0 else 0)
-			if new_cost <  current_cost and capacityRemaining[w] > customerSizes[c]:
-				solution[c] = w
-				capacityRemaining[w] -= customerSizes[c]
-				usage_count[w] +=1
+			warehouse = improvements[w]
+			warehouse_gain = sum(warehouse)
+
+			if warehouse_gain > warehouses[w][1]:
+				improv = [(improvements[w][c],c) for c in range(customerCount)]
+				improv.sort()
+				improv = improv[::-1]
+
+				for c in xrange(customerCount):
+					if warehouse[improv[c][1]] == 0:
+						break
+					if capacityRemaining[w] > customerSizes[improv[c][1]]:
+						improved = True
+						if solution[improv[c][1]] != -1: usage_count[solution[improv[c][1]]] -= 1
+						usage_count[w] += 1
+						solution[improv[c][1]] = w
+						capacityRemaining[w] -= customerSizes[improv[c][1]]
+				#print warehouse_gain, warehouses[w][1], w
+				if improved: break
+		
+		used = [0]*warehouseCount
+		for wa in solution:
+			used[wa] = 1
+
+		# calculate the cost of the solution
+		obj = sum([warehouses[x][1]*used[x] for x in range(0,warehouseCount)])
+		for c in range(0, customerCount):
+			obj += customerCosts[c][solution[c]]
+		print iteration, obj
+	
+		if not improved:
+			break
 
 
-	used = [0]*warehouseCount
-	for wa in solution:
-		used[wa] = 1
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	###################################################
 	################  Problem solving end    ##########
 	###################################################
+
+	used = [0]*warehouseCount
+	for wa in solution:
+		used[wa] = 1
 
 	# calculate the cost of the solution
 	obj = sum([warehouses[x][1]*used[x] for x in range(0,warehouseCount)])
